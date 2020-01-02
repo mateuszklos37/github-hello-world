3fda02a7aa65cd200ad5a69c155bd75bc51f2080
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2020-01-02 17:28:30 +0100

    Added new class and modified old one

--- src/main/java/com/ghw/githubhelloworld/FirstClass.java
+++ src/main/java/com/ghw/githubhelloworld/FirstClass.java
@@ -1,4 +1,10 @@
 package com.ghw.githubhelloworld;
 
 public class FirstClass {
+    private String name;
+    private int age;
+
+    public void sayHello(){
+        System.out.println("Just saying hello!");
+    }
 }
