de97b984dcab7874a18ca0e852ee8f0f03ce0811
mateuszklos37@gmail.com <mateuszklos37@gmail.com>
2020-01-06 15:24:22 +0100

    Class to approve

---
+++ src/main/java/com/ghw/githubhelloworld/GitRequestApprove.java
@@ -0,0 +1,10 @@
+package com.ghw.githubhelloworld;
+
+public class GitRequestApprove {
+    private String attribute;
+
+    public void showAttribute(){
+        attribute = "I'm saying hello!";
+        System.out.println(attribute);
+    }
+}
